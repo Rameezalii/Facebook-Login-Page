5b3c1f6aab570cbeec722d310469312f7d12bd80
Rameez Ali <ra2252252@gmail.com>
2024-07-22 00:55:18 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code can be used for form validation, handling button clicks, or any other interactivity
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Form submitted');
+});
